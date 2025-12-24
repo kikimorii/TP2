7bcac8c51c935b6ad60bedf96f575a0dfe966f69
Oleg <fateev010405@gmail.com>
2025-12-24 13:05:05 +0300

    final changes

--- main.cpp
+++ main.cpp
@@ -3,124 +3,128 @@
 #include <vector>
 #include <string>
 
+
 void FirstInterrogativeThenExclamation()
 {
-    std::ifstream file("sentences.txt");
-    std::vector<std::string> arrOfInterrogativeSentences;
-    std::vector<std::string> arrOfExclamationSentences;
-    char currentSymbol;
-    std::string currentSentence;
-    file >> currentSymbol;
-    currentSentence += currentSymbol;
-    while (!file.eof())
-    {
-        file >> currentSymbol;
-        currentSentence += currentSymbol;
-        if (currentSymbol == '?' && currentSentence != "?")
-        {
-            arrOfInterrogativeSentences.emplace_back(currentSentence);
-            currentSentence = "";
-        }
-        else if (currentSymbol == '!' && currentSentence != "!")
-        {
-            arrOfExclamationSentences.emplace_back(currentSentence);
-            currentSentence = "";
-        }
-    }
-    
-    for (std::string& string : arrOfInterrogativeSentences)
-    {
-        std::cout << string << std::endl;
-    }
-    for (std::string& string : arrOfExclamationSentences)
-    {
-        std::cout << string << std::endl;
-    }
+   std::ifstream file("sentences.txt");
+   std::vector<std::string> arrOfInterrogativeSentences;
+   std::vector<std::string> arrOfExclamationSentences;
+   char currentSymbol;
+   std::string currentSentence;
+   file >> currentSymbol;
+   currentSentence += currentSymbol;
+   while (!file.eof())
+   {
+	  file >> currentSymbol;
+	  currentSentence += currentSymbol;
+	  if (currentSymbol == '?' && currentSentence != "?")
+	  {
+		 arrOfInterrogativeSentences.emplace_back(currentSentence);
+		 currentSentence = "";
+	  }
+	  else if (currentSymbol == '!' && currentSentence != "!")
+	  {
+		 arrOfExclamationSentences.emplace_back(currentSentence);
+		 currentSentence = "";
+	  }
+   }
+   
+   for (std::string& string : arrOfInterrogativeSentences)
+   {
+	  std::cout << string << std::endl;
+   }
+   for (std::string& string : arrOfExclamationSentences)
+   {
+	  std::cout << string << std::endl;
+   }
 }
+
 
 int main()
 {
-    std::cout << "Commands:" << std::endl;
-    std::cout << "1 [firstName] [lastName] [day] [month] [year] [zodiac] : create new sign" << std::endl;
-    std::cout << "2 [index] : delete sign by index" << std::endl;
-    std::cout << "3 [index] [firstName] [lastName] [day] [month] [year] [zodiac] : update sign by index" << std::endl;
-    std::cout << "4 [index] : print sign by index" << std::endl;
-    std::cout << "5 [zodiac] : print all signs with sign.zodiac=zodiac" << std::endl;
-    std::cout << "6 : print all signs" << std::endl;
-    std::cout << "7 [filename] : display all sentences from file with name=[filename]" << std::endl;
-    std::cout << "8 : quit" << std::endl;
-    
-    SignContainer signContainer;
-    int command = 0;
-    while (command != 8)
-    {
-        std::cin >> command;
-        switch (command)
-        {
-            case 1:
-            {
-                std::string firstName;
-                std::string lastName;
-                int day = 0;
-                int month = 0;
-                int year = 0;
-                int birthday[3];
-                int zodiac = 0;
-                std::cin >> firstName >> lastName >> birthday[0] >> birthday[1] >> birthday[2] >> zodiac;
-                Sign* sign = new Sign(firstName, lastName, birthday, zodiac);
-                signContainer.Add(sign);
-                break;
-            }
-            case 2:
-            {
-                int index;
-                std::cin >> index;
-                signContainer.Delete(index);
-                break;
-            }
-            case 3:
-            {
-                int index;
-                std::string firstName;
-                std::string lastName;
-                int day = 0;
-                int month = 0;
-                int year = 0;
-                int birthday[3];
-                int zodiac = 0;
-                std::cin >> index >> firstName >> lastName >> birthday[0] >> birthday[1] >> birthday[2] >> zodiac;
-                Sign* sign = signContainer.GetSign(index);
-                sign->SetFirstName(firstName);
-                sign->SetLastName(lastName);
-                sign->SetBirthday(birthday);
-                sign->SetZodiac(zodiac);
-                break;
-            }
-            case 4:
-            {
-                int index;
-                std::cin >> index;
-                signContainer.PrintByIndex(index);
-                break;
-            }
-            case 5:
-            {
-                int zodiac;
-                std::cin >> zodiac;
-                signContainer.PrintWithZodiac(zodiac);
-                break;
-            }
-            case 6:
-            {
-                signContainer.Print();
-                break;
-            }
-            case 7:
-            {
-                FirstInterrogativeThenExclamation();
-                break;
-            }
-        }
-    }
-    return 0;
+   std::cout << "Commands:" << std::endl;
+   std::cout << "1 [firstName] [lastName] [day] [month] [year] [zodiac] : create new sign" << std::endl;
+   std::cout << "2 [index] : delete sign by index" << std::endl;
+   std::cout << "3 [index] [firstName] [lastName] [day] [month] [year] [zodiac] : update sign by index" << std::endl;
+   std::cout << "4 [index] : print sign by index" << std::endl;
+   std::cout << "5 [zodiac] : print all signs with sign.zodiac=zodiac" << std::endl;
+   std::cout << "6 : print all signs" << std::endl;
+   std::cout << "7 [filename] : display all sentences from file with name=[filename]" << std::endl;
+   std::cout << "8 : quit" << std::endl;
+
+   SignContainer signContainer;
+
+   int command = 0;
+   while (command != 8)
+   {
+	  std::cin >> command;
+	  switch (command)
+	  {
+		 case 1:
+		 {
+			std::string firstName;
+			std::string lastName;
+			int day = 0;
+			int month = 0;
+			int year = 0;
+			int birthday[3];
+			int zodiac = 0;
+			std::cin >> firstName >> lastName >> birthday[0] >> birthday[1] >> birthday[2] >> zodiac;
+			Sign* sign = new Sign(firstName, lastName, birthday, zodiac);
+			signContainer.Add(sign);
+			break;
+		 }
+		 case 2:
+		 {
+			int index;
+			std::cin >> index;
+			signContainer.Delete(index);
+			break;
+		 }
+		 case 3:
+		 {
+			int index;
+			std::string firstName;
+			std::string lastName;
+			int day = 0;
+			int month = 0;
+			int year = 0;
+			int birthday[3];
+			int zodiac = 0;
+			std::cin >> index >> firstName >> lastName >> birthday[0] >> birthday[1] >> birthday[2] >> zodiac;
+			Sign* sign = signContainer.GetSign(index);
+			sign->SetFirstName(firstName);
+			sign->SetLastName(lastName);
+			sign->SetBirthday(birthday);
+			sign->SetZodiac(zodiac);
+			break;
+		 }
+		 case 4:
+		 {
+			int index;
+			std::cin >> index;
+			signContainer.PrintByIndex(index);
+			break;
+		 }
+		 case 5:
+		 {
+			int zodiac;
+			std::cin >> zodiac;
+			signContainer.PrintWithZodiac(zodiac);
+			break;
+		 }
+		 case 6:
+		 {
+			signContainer.Print();
+			break;
+		 }
+		 case 7:
+		 {
+			FirstInterrogativeThenExclamation();
+			break;
+		 }
+	  }
+   }
+
+   return 0;
 }
--- sign.h
+++ sign.h
@@ -1,50 +1,61 @@
 #include <string>
 #include <iostream>
 
+
 class Sign
 {
 private:
-    Sign* nextSign;
-    std::string firstName;
-    std::string lastName;
-    int birthday[3];
-    int zodiac;
+   Sign* nextSign;
+
+   std::string firstName;
+   std::string lastName;
+   int birthday[3];
+   int zodiac;
+
 public:
-    Sign();
-    Sign(const std::string firstName_, const std::string lastName_, const int(&birthday_)[3], const int zodiac_);
-    Sign(const Sign& sign);
-    ~Sign();
-    Sign* GetNextSign();
-    void SetNextSign(Sign* sign);
-    std::string GetFirstName() const;
-    std::string GetLastName() const;
-    void GetBirthday(int* day, int* month, int* year) const;
-    int GetZodiac() const;
-    void SetFirstName(const std::string& firstName_);
-    void SetLastName(const std::string& lastName_);
-    void SetBirthday(const int(&birthday_)[3]);
-    void SetZodiac(const int zodiac_);
+   Sign();
+   Sign(const std::string firstName_, const std::string lastName_, const int(&birthday_)[3], const int zodiac_);
+   Sign(const Sign& sign);
+   ~Sign();
+
+   Sign* GetNextSign();
+   void SetNextSign(Sign* sign);
+
+   std::string GetFirstName() const;
+   std::string GetLastName() const;
+   void GetBirthday(int* day, int* month, int* year) const;
+   int GetZodiac() const;
+
+   void SetFirstName(const std::string& firstName_);
+   void SetLastName(const std::string& lastName_);
+   void SetBirthday(const int(&birthday_)[3]);
+   void SetZodiac(const int zodiac_);
 };
+
 
 std::ostream& operator<<(std::ostream& outStream, const Sign& sign);
 std::istream& operator>>(std::istream& inStream, Sign& sign);
 
+
 class SignContainer
 {
 private:
-    Sign* firstSign;
-    Sign* lastSign;
-    int length;
+   Sign* firstSign;
+   Sign* lastSign;
+   int length;
+
 public:
-    SignContainer();
-    SignContainer(Sign* sign);
-    SignContainer(const SignContainer& signContainer);
-    ~SignContainer();
-    void Add(Sign* sign);
-    void Delete(int index);
-    void Print() const;
-    void PrintByIndex(int index) const;
-    void PrintWithZodiac(int zodiac) const;
-    Sign* GetFirstSign() const;
-    Sign* GetSign(int index) const;
+   SignContainer();
+   SignContainer(Sign* sign);
+   SignContainer(const SignContainer& signContainer);
+   ~SignContainer();
+
+   void Add(Sign* sign);
+   void Delete(int index);
+   void Print() const;
+   void PrintByIndex(int index) const;
+   void PrintWithZodiac(int zodiac) const;
+
+   Sign* GetFirstSign() const;
+   Sign* GetSign(int index) const;
 };
